884813cfa1f06a6000bd8fad9a8ac4bab32b27bf
Roshan Parmar <roshansinghparmar77@gmail.com>
2025-09-02 22:42:00 +0530

    Added dynamic port number

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const connectedToDb = require("./db/db");
 const app = require('./src/app');
 
 connectedToDb()
-
-app.listen("3000",()=>{
-    console.log("server started running");
+const PORT = process.env.PORT || 3000; 
+// 3000 is just fallback for local dev
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
